bd01c828244961fd53ee0cec31a06ee425b7294b
Jake Schaefbauer <jschaefmn@gmail.com>
2022-06-02 20:13:05 -0500

    final commit/fixed js coloring on descriptions

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,39 +13,41 @@ $(document).ready(function () {
   function timeline() {
     // check for amount of hours
     var currentTime = moment().hour();
-
     // loop through the time blocks
-    $('.time-block').each(function () {
-      var hourBlock = parseInt($(this).attr('id').split('hour')[1]);
+    $(".time-block").each(function () {
+      var hourBlock = parseInt($(this).attr("id"));
+
 
       // check time and change background depending on if past, present, or future
       if (hourBlock < currentTime) {
-        $(this).removeClass('present');
-        $(this).removeClass('future');
-        $(this).addClass('past');
-      } else if (hourBlock === currentTime) {
-        $(this).removeClass('past');
-        $(this).removeClass('future');
-        $(this).addClass('present');
-      } else {
-        $(this).removeClass('past');
-        $(this).removeClass('present');
-        $(this).addClass('future');
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      }
+      else if (hourBlock === currentTime) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+      }
+      else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
       }
     });
   };
 
   // load items from localStorage
-  $('#1-hour .description').val(localStorage.getItem('1-hour'));
-  $('#2-hour .description').val(localStorage.getItem('2-hour'));
-  $('#3-hour .description').val(localStorage.getItem('3-hour'));
-  $('#4-hour .description').val(localStorage.getItem('4-hour'));
-  $('#5-hour .description').val(localStorage.getItem('5-hour'));
-  $('#6-hour .description').val(localStorage.getItem('6-hour'));
-  $('#7-hour .description').val(localStorage.getItem('7-hour'));
-  $('#8-hour .description').val(localStorage.getItem('8-hour'));
-  $('#9-hour .description').val(localStorage.getItem('9-hour'));
-  $('#10-hour .description').val(localStorage.getItem('10-hour'));
+  $('#1-hour .description').val(localStorage.getItem('8-hour'));
+  $('#2-hour .description').val(localStorage.getItem('9-hour'));
+  $('#3-hour .description').val(localStorage.getItem('10-hour'));
+  $('#4-hour .description').val(localStorage.getItem('11-hour'));
+  $('#5-hour .description').val(localStorage.getItem('12-hour'));
+  $('#6-hour .description').val(localStorage.getItem('13-hour'));
+  $('#7-hour .description').val(localStorage.getItem('14-hour'));
+  $('#8-hour .description').val(localStorage.getItem('15-hour'));
+  $('#9-hour .description').val(localStorage.getItem('16-hour'));
+  $('#10-hour .description').val(localStorage.getItem('17-hour'));
 
   timeline()
 });
